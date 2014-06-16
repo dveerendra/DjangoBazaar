ac4c8c39e1d59c86f88a19829ddc9dc57319a69b
DeWaster <joonas.reinikka@gmail.com>
2014-06-16 09:24:13 +0300

    Initial Prototype-branch commit.
    - Renamed Bmodels
    - Made urls.py

--- bazaar/Bmodels/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from Bmodels.models import Item, UserInfo
-# Register your models here.
-
-class BmodelAdmin(admin.ModelAdmin):
-    admin.site.register(UserInfo)
-    admin.site.register(Item)
