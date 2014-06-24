462fdc21d465ca4190205b843836e666ce64cf2c
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2014-06-24 11:12:43 +0000

    added collection models

--- bazaar/shop/models.py
+++ bazaar/shop/models.py
@@ -22,9 +22,19 @@ class UserInfo(models.Model):
 # If this is meant to generate tokens for OAuth2 used in API, the rest-framework
 # will provide generator.
 """
-class TokenGen(models.Model):
+class TokenGen(models.Model):ss
     Token = models.CharField(max_length=28)
 """
+
+#Collection information is stored in this table
+class MaterialCollections(models.Model):
+    cTitle = models.CharField(max_length=255)
+    createdAt =  models.DateTimeField(auto_now_add=True)
+    lastModified = models.DateField(auto_now_add=True)
+
+class hasCollection(models.Model):
+    parentID = models.ForeignKey(MaterialCollections)
+    childID = models.ForeignKey(MaterialCollections)
 
 # fill in all the details we created for the API-definition
 # and discuss the rating-thumb thing
@@ -41,6 +51,7 @@ class MaterialItem(models.Model):
     numberOfRatings = models.IntegerField(default=0)
     numberOfLikes = models.IntegerField(default=0)
     author = models.ForeignKey(User)
+    collectionId = models.ForeignKey(MaterialCollections)
 
     def __str__(self):
         return self.title
@@ -52,19 +63,20 @@ class MaterialItem(models.Model):
 # this is folksonomical tag right?
 # needs modifications to be usable with Django's own user-system
 class Tags(models.Model):
-    name= models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
     createdAt = models.DateTimeField(auto_now_add=True)
     lastModified = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User,related_name='user_id')
     lastModifiedBy = models.ForeignKey(User,related_name='modified_by_user_id')
+    ItemTags = models.ManyToManyField(MaterialItem)
 
     def __str__(self):
         return self.Name
 
 
-class ItemTags(models.Model):
-    itemId = models.ForeignKey(MaterialItem)
-    tagsId = models.ForeignKey(Tags)
+#class ItemTags(models.Model):
+ #   itemId = models.ForeignKey(MaterialItem)
+  #  tagsId = models.ForeignKey(Tags)
 
 
 #note the separation of the comments and ratings:
@@ -76,13 +88,21 @@ class Ratings(models.Model):
     def __str__(self):
         return self.Rate
 
+class ItemRatings(models.Model):
+    itemId = models.ForeignKey(MaterialItem)
+    ratingsID = models.ForeignKey(Ratings)
+
+
 class Comment(models.Model):
     commentText = models.TextField(max_length=8000)
     author = models.ForeignKey(User)
 
-class ItemRatings(models.Model):
+    def __str__(self):
+        return self.commentText
+
+class ItemComments(models.Model):
     itemId = models.ForeignKey(MaterialItem)
-    ratingsID = models.ForeignKey(Ratings)
+    commentsID = models.ForeignKey(Comment)
 
 #
 class ProviderOrganization(models.Model):
