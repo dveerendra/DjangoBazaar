39fed0de5932a6a1ebb9d29eddc7ddb573fd813d
Lucas Machado <machadolucas@me.com>
2014-06-26 10:17:14 +0300

    Database password reverted

--- bazaar/settings/development.py
+++ bazaar/settings/development.py
@@ -26,8 +26,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bazaar',
-        'USER': 'root',
-        'PASSWORD': 'satura',
+        'USER': 'bazaar',
+        'PASSWORD': 'koira',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
